39199496816b14927b6b248269da5eb80a78769b
keromagdy80 <keromagdy807@gmail.com>
2024-08-24 22:08:20 +0300

    Splash$ onboarding screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:e_commerce/login.dart';
+import 'package:e_commerce/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'navigations.dart';
 
 
 void main() {
-  runApp( const MyApp());
+  runApp(  MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+    MyApp({super.key});
+ThemeData myTheme=ThemeData
+(
+primaryColor:  const Color(0xff40BFFF),
 
-  // This widget is the root of your application.
+) ;
+  
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
+    return   MaterialApp(theme: myTheme,
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home:const  SplashScreen(),
     );
   }
 }
